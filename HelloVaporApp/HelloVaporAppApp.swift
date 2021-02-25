ac027c4daa5c52021550ae0bc81483065c454c7c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-25 15:26:19 +0200

    Initial Commit

---
+++ HelloVaporApp/HelloVaporAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloVaporAppApp.swift
+//  HelloVaporApp
+//
+//  Created by Ramill Ibragimov on 25.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloVaporAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
